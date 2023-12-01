a5a56f578ed392e4143a6ad7aad9925592aace20
Jerome <jeromelesne@gmail.com>
2023-12-01 16:00:13 +0100

    work but no animation at second attempt

--- script.js
+++ script.js
@@ -1,13 +1,51 @@
 // function that take weight (kg) and size (cm) as argument
 let imcCalculator = (weight, size) => {
     size /= 100;
-    return Math.round(weight / (size * size));
+    return (weight / (size * size)).toFixed(1);
 };
 
 let dataForm = document.querySelector("#dataForm");
+let test = "test";
+let currentPosition = "0deg";
+const r = document.querySelector(":root");
 
-dataForm.addEventListener("submit", () => {
+dataForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const needle = document.querySelector("#needle");
+    needle.classList.remove("firstAnim");
+    needle.classList.remove("position");
+
     let size = document.querySelector("#size");
     let weight = document.querySelector("#weight");
-    alert(imcCalculator(weight.value, size.value));
+    let imc = imcCalculator(weight.value, size.value);
+    console.log("imc :", imc);
+
+    console.log("current", currentPosition);
+    r.style.setProperty("--needle-current-position", currentPosition);
+
+    let position = "";
+    if (imc < 18.5) {
+        position = "-72deg";
+    } else if (imc >= 18.5 && imc < 25) {
+        position = "-36deg";
+    } else if (imc >= 25 && imc < 30) {
+        position = "0deg";
+    } else if (imc >= 30 && imc < 35) {
+        position = "36deg";
+    } else {
+        position = "72deg";
+    }
+
+    console.log("position ", position);
+    r.style.setProperty("--needle-position", position);
+    needle.classList.toggle("position");
+
+    currentPosition = position;
+
+    console.log(
+        r.style.getPropertyValue("--needle-position"),
+        r.style.getPropertyValue("--needle-current-position"),
+    );
+    console.log("-----------------");
 });
