378e35931d68661f96c89e7f6fc087b27852ccc1
Jerome <jeromelesne@gmail.com>
2023-12-04 10:22:12 +0100

    implemented message

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ let imcCalculator = (weight, size) => {
 let dataForm = document.querySelector("#dataForm");
 let test = "test";
 let currentPosition = "0deg";
+let message = document.querySelector("#message");
 
 dataForm.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -25,14 +26,33 @@ dataForm.addEventListener("submit", (e) => {
     let position = "";
     if (imc < 18.5) {
         position = "-72deg";
+        message.innerHTML =
+            "Va falloir penser à prendre un abonnement chez macdo";
+        message.style.color = "#1DA614";
+        message.style.textAlign = "center";
     } else if (imc >= 18.5 && imc < 25) {
         position = "-36deg";
+        message.innerHTML = "Nickel, continue comme ca mon petit bonhomme";
+        message.style.color = "#9FCC00";
+        message.style.textAlign = "center";
     } else if (imc >= 25 && imc < 30) {
         position = "0deg";
+        message.innerHTML =
+            "Ca va mais va falloir surveillert tout ca quand même, un peu de sport ca fait pas de mal non plus";
+        message.style.color = "#FFF002";
+        message.style.textAlign = "center";
     } else if (imc >= 30 && imc < 35) {
         position = "36deg";
+        message.innerHTML =
+            "Oulala je veux pas être grossophobe mais bon on va peut être arrêter de se mentir et démarrer un ptit régime rapidos";
+        message.style.color = "#FEAF02";
+        message.style.textAlign = "center";
     } else {
         position = "72deg";
+        message.innerHTML =
+            "Dans pas longtemps je vais commencer à entrer en orbite autour de toi";
+        message.style.color = "#FF2900";
+        message.style.textAlign = "center";
     }
 
     needle.style.transform = `rotate(${position})`;
